c84e6db4a7d5c7add67125043e0b0c7158999b18
Saugata Dutta <saugata.dutta@gmail.com>
2018-05-09 23:22:05 +0530

    Added new entries for postgres and hibernate

--- src/main/java/com/tgt/mkt/cam/StarterApplication.java
+++ src/main/java/com/tgt/mkt/cam/StarterApplication.java
@@ -1,13 +1,17 @@
 package com.tgt.mkt.cam;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by z083387 on 8/3/17.
  */
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableSwagger2
 public class StarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class, args);
